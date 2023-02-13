ac6477399e26a9b3c6cf287ec7d8775d85b6f693
Reupen Shah <reupen@users.noreply.github.com>
2023-02-13 20:57:33 +0000

    Remove unused dependency

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,14 @@
+const cssnano = require("cssnano");
 const postcssPresetEnv = require("postcss-preset-env");
 const atImport = require("postcss-import");
 const postcssCustomMedia = require("postcss-custom-media");
-const path = require("node:path");
-const url = require("postcss-url");
 
 module.exports = {
   plugins: [
     atImport(),
     postcssCustomMedia(),
     postcssPresetEnv({ stage: 1 }),
-    // url({
-    //     url: "copy",
-    // basePath: path.resolve('node_modules/inter-ui'),
-    // // dir to copy assets
-    // assetsPath: 'static',
-    // // using hash names for assets (generates from asset content)
-    // useHash: true
-    //}),
-    require("cssnano")({
+    cssnano({
       preset: "default",
     }),
   ],
